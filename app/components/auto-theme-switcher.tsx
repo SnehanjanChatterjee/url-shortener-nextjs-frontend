19189f9e5f725db32177d0ad55c64c6321d71ebf
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2025-01-20 01:10:00 +0530

    Use auto theme switcher if only user has no saved preference in local storage

--- app/components/auto-theme-switcher.tsx
+++ app/components/auto-theme-switcher.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import { useEffect } from "react";
+import { THEMES } from "../constants/UrlShortenerConstants";
 
 export default function AutoThemeSwitcher() {
   useEffect(() => {
-    const currentHour = new Date().getHours();
-    const theme = (currentHour >= 18 && currentHour < 6) ? "dark" : "wireframe";
-    
-    // Set the data-theme attribute on the <html> tag
-    document.documentElement.setAttribute("data-theme", theme);
+    const savedTheme = localStorage.getItem('isDarkMode');
+    if (savedTheme === null) {
+      const currentHour = new Date().getHours();
+      const theme = (currentHour >= 18 && currentHour < 6) ? THEMES.dark : THEMES.wireframe;
+      
+      // Set the data-theme attribute on the <html> tag
+      document.documentElement.setAttribute("data-theme", theme);
+    }
   }, []);
 
   return null; // No need to render anything, just applying the theme
