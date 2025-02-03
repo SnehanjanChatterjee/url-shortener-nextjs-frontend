763a7b1ccf28d0d17df1877d3371b7e856aa2b12
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2025-02-03 20:34:50 +0530

    Added GitHub OAuth provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import Google from "@auth/core/providers/google";
+import GitHub from "@auth/core/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
+    providers: [Google, GitHub],
 })
