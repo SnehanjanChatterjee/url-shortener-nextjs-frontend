f37be8a6148b055a3f5bc0172bb3a342bb9b52ab
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2025-02-09 04:54:22 +0530

    Modified BACKEND_ENDPOINT to use localhost if NEXT_PUBLIC_BACKEND_CLOUD_ENDPOINT not available

--- app/constants/UrlShortenerConstants.ts
+++ app/constants/UrlShortenerConstants.ts
@@ -1,5 +1,5 @@
 // API Constants
-const BACKEND_ENDPOINT: string  = process.env.NEXT_PUBLIC_BACKEND_CLOUD_ENDPOINT ?? "";
+const BACKEND_ENDPOINT: string  = process.env.NEXT_PUBLIC_BACKEND_CLOUD_ENDPOINT ?? "http://localhost:8080";
 const BACKEND_REST_PATH: string  = process.env.NEXT_PUBLIC_BACKEND_REST_PATH ?? "";
 const URL_SHORTENER_BASE_ENDPOINT: string = BACKEND_ENDPOINT.concat(BACKEND_REST_PATH);
 export const URL_SHORTENER_GENERATE_ENDPOINT: string = URL_SHORTENER_BASE_ENDPOINT.concat('/url/generate');
